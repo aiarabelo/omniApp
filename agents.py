baf51ae530b271ad42ea8b56d4c97f9636513af5
aiarabelo <49424364+aiarabelo@users.noreply.github.com>
2019-06-22 13:28:45 -0700

    Added functon which checks for short answers

--- agents.py
+++ agents.py
@@ -13,12 +13,23 @@ class Agent:
         self.driver = webdriver.Chrome(options=options, 
             executable_path=self.chrome_executable_path)
 
-    '''
-    Dictionary from question label to web element
-    '''
-
-    def getQuestionDict():
-        raise NotImplementedError
+    """
+    FUNCTION: Fills out the application page
+    questionPair: a dictionary containing questions as they key, 
+                  and the corresponding WebElements as values
+    userData: a dictionary containing the questions as the key, 
+              and the answers as values
+    Returns questionPair, a dictionary containing questions and its corresponding webelement
+    """
+    def getQuestionDict(self):
+        self.get("file:///C:/Users/aiarabelo/Desktop/Projects/omniApp/testpage.html")
+        questions = self.driver.find_elements_by_class_name("application-question")
+        questionPair = {}
+        for question in questions:
+            questionLabel = question.find_element_by_class_name("application-label")
+            questionPair[questionLabel.text.split("\n")[0]] = question
+        return questionPair
+        
 
 class LeverAgent(Agent):
     def __init__(self, headless, chrome_executable_path):
@@ -28,25 +39,6 @@ class LeverAgent(Agent):
 
     def get(self, url):
         self.driver.get(url)
-
-    """
-    FUNCTION: Fills out the application page
-    questionPair: a dictionary containing questions as they key, 
-                  and the corresponding WebElements as values
-    userData: a dictionary containing the questions as the key, 
-              and the answers as values
-    Returns questionPair, a dictionary containing questions and its corresponding webelement
-    """
-
-    def getQuestionDict(self):
-        #self.get("file:///C:/Users/aiarabelo/Desktop/Projects/omniApp/testpage.html")
-        self.get("file:///C:/Users/aiarabelo/Desktop/Projects/omniApp/testpage2.html")
-        questions = self.driver.find_elements_by_class_name("application-question")
-        questionPair = {}
-        for question in questions:
-            questionLabel = question.find_element_by_class_name("application-label")
-            questionPair[questionLabel.text.split("\n")[0]] = question
-        return questionPair
     
     """
     FUNCTION: Fills out the application page
@@ -56,38 +48,35 @@ class LeverAgent(Agent):
               and the answers as values
     
     """
+     
+
     def autoInputQuestion(self, questionPair, userData):
         userDataSet = set(userData.keys())
         questionPairSet = set(questionPair.keys())
-
         for key in questionPair.keys():
             inputAnswer = questionPair[key].find_elements_by_tag_name("input")
             selectAnswer = questionPair[key].find_elements_by_tag_name("select")
             textareaAnswer = questionPair[key].find_elements_by_tag_name("textarea")
-
             if key not in userData.keys():
-                continue
-
-            elif len(inputAnswer) == 1:  
+                pass
+            if len(inputAnswer) == 1:  
                 inputAnswer[0].send_keys(userData[key])
-            
             elif len(inputAnswer) > 1:  
                 inputType = inputAnswer[0].get_attribute("type")
                 if inputType == "radio":
                     self.radioInput(inputAnswer, userData[key])
                 elif inputType == "checkbox": 
-                    self.checkboxInput(inputAnswer, userData[key]) 
+                    self.checkboxInput(inputAnswer, userData[key])             
             
-            elif len(textareaAnswer) == 1:  
-                textareaAnswer[0].send_keys(userData[key])
-
+                #textareaQuestion = inputAnswer[0].get_attribute("placeholder")
+                #print(textareaQuestion)  
+                #textareaAnswer[0].send_keys(userData[key])
             elif len(selectAnswer) != 0: 
                 select_element = Select(selectAnswer[0])
                 select_element.select_by_visible_text(userData[key])
-
             else: 
-                continue
-                print("You have more things to do")     
+                self.checkForShortAnswers(textareaAnswer)
+           
 
     """
     FUNCTION: Fills out a radio input; used in the function "autoInputQuestion"
@@ -111,6 +100,19 @@ class LeverAgent(Agent):
             choices = [choice.get_attribute("value") for choice in inputAnswer]
             index = choices.index(userAnswer)
             self.driver.execute_script("arguments[0].click();", inputAnswer[index])
+    
+    """
+    FUNCTION: Returns True if there are short answer questions, and breaks the function otherwise 
+    checker: textareaAnswer from autoInputQuestion(); 1 if there are questions, 0 otherwise
+    
+    """
+    def checkForShortAnswers(self, checker):
+        if len(checker) == 0: 
+            print("False")
+            return False
+        elif len(checker) == 1:
+            print("True")
+            return True
 
 test = LeverAgent(False, r"C:\Users\aiarabelo\Desktop\Projects\omniApp\chromedriver.exe")
 
@@ -136,17 +138,18 @@ userData = {
             "Will you now or in the future require sponsorship for employment visa status" : "Yes",
             "When are you seeking to begin a full-time position?" : "Immediately", 
             "Do you currently, or in the future will you, require sponsorship to legally work in the United States?" : "Yes",
+            #TODO generalize; make sure additional information is actually cover letter 
             "Were you referred to Rigetti?" : ["Yes"], 
             "If so, by whom?" : "Daniel Setiawan",
             "Language Skill s Check all that apply" : ["English (ENG)"],
             "Where are you applying from" : "United States [USA]",
-            "How did you hear about this job?" : "None of the above/Other",
+            "How did you hear about this job?" : "LinkedIn",
             #"Which university are you currently attending or did you last attend Please select Other School Not Listed if your school is not listed" : "University of California - Berkeley",
             "Please tell us how you heard about this opportunity" : "Other",
             "What has been your favorite project or proudest accomplishment Why" : "TEST TEST TEST",
             "Gender" : "Female",
             "Race" : "Asian (Not Hispanic or Latino)",
-            "Veteran status" : "I am not a protected veteran",
+            "Veteran status" : "I am not a veteran",
             "I certify the information and answers provided by me within this application are true and correct." : "I Accept / I Agree",
             "Disability status" : "No, I don't have a disability"
 }
