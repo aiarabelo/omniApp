a3caf745deffe9c22e7fdaf03e8463b4cb5880d2
aiarabelo <49424364+aiarabelo@users.noreply.github.com>
2019-07-02 04:25:45 -0700

    User signups with hashed pws

--- flask_site/__init__.py
+++ flask_site/__init__.py
@@ -1,13 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from wtforms import Form, BooleanField, StringField, PasswordField, validators
-import psycopg2
-from sqlalchemy import create_engine
-from sqlalchemy import Table, Column, String, MetaData
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'a2f8836ec3c14d304d0422e18a0e4366' 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:l1pt0n@localhost:5432/omniApp'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from flask_site import routes
