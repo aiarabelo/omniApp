515375513fc9e1d6e172ac5115440bd573a8f33f
aiarabelo <49424364+aiarabelo@users.noreply.github.com>
2019-07-05 00:28:16 -0700

    Profile pictures added, email and username can be updated

--- flask_site/forms.py
+++ flask_site/forms.py
@@ -1,7 +1,9 @@
 from flask_wtf import FlaskForm
+from flask_login import current_user
 from wtforms import Form, StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flask_site.models import User
+
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username',
@@ -43,4 +45,33 @@ class LoginForm(FlaskForm):
                               validators =[DataRequired()])
     remember = BooleanField('Remember me')
     submit = SubmitField('Login')
-    
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username',
+                            validators = [DataRequired(), Length(min=3, max=20)])
+    email = StringField('Email',
+                         validators = [DataRequired(), Email()])
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):     
+      """
+      FUNCTION: Updates username if entered update is different from current
+      username.data: username selected that is different from entered in the form 
+      current_user.username: the current username of the user, as seen from the db
+      If "user" is none, user email is unique and won't trigger the error. Otherwise, will raise a validation error
+      """
+      if username.data != current_user.username: 
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+          raise ValidationError('Username is taken. Please choose another one.')
+
+    def validate_email(self, email):
+      """
+      FUNCTION: Updates email if entered update is different from current
+      email.data: email selected that is different from entered in the form 
+      current_user.email: the current email of the user, as seen from the db If "user" is none, user email is unique and won't trigger the error. Otherwise, will raise a validation error
+      """
+      if email.data != current_user.email:
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+          raise ValidationError('That email is taken. Please choose another one.')
--- flask_site/routes.py
+++ flask_site/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, url_for, flash, redirect, request
 from flask_site import app, db, bcrypt
-from flask_site.forms import RegistrationForm, LoginForm
+from flask_site.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from flask_site.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 
@@ -80,7 +80,20 @@ def logout():
     logout_user()
     return redirect(url_for('home'))
 
-@app.route("/account")
+@app.route("/account", methods=['GET', 'POST'])
 @login_required
 def account():
-    return render_template('account.html', title = 'Account')
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        print(current_user)
+        db.session.commit()
+        flash("Your account has been updated.", 'success')
+        return redirect(url_for('account'))
+    elif request.method == "GET":
+        print('hi')
+        form.username.data = current_user.username 
+        form.email.data = current_user.email 
+    image_file = url_for('static', filename='profile_pics/'+ current_user.image_file)
+    return render_template('account.html', title = 'Account', image_file = image_file, form = form)
