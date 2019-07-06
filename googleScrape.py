5985816d57bc227fecb69ab9ac9a687638a14839
aiarabelo <49424364+aiarabelo@users.noreply.github.com>
2019-07-05 20:30:23 -0700

    Finished companyURL method

--- googleScrape.py
+++ googleScrape.py
@@ -1,17 +1,18 @@
 from googlesearch import search
 import time
 
-atsURLS = ['boards.greenhouse.io', 'jobs.lever.co']
+atsURLs = ['boards.greenhouse.io', 'jobs.lever.co']
 
 class webScraper:
-    def companyURL(self, atsURL):
+    def companyURL(self, ats):
         i = 1
-        with open(atsURL + ".txt", "a+") as companies:
+        with open(ats + ".txt", "a+") as companies:
             for url in search('site:' + atsURL, stop = 25000):
                 companies.write(url+"\n")
                 i+=1
                 if i % 10 == 0:
                     time.sleep(40)
-                print(url)
+                print("(" + atsURL + ") " +  url)
 
-webScraper().companyURL(atsURLS[0])
+for atsURL in atsURLs:
+    webScraper().companyURL(atsURL)
