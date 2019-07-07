9b35977926c7893575d78bd02f99cb6068b97754
aiarabelo <49424364+aiarabelo@users.noreply.github.com>
2019-07-06 18:06:11 -0700

    Cleaner code

--- googleScrape.py
+++ googleScrape.py
@@ -8,26 +8,30 @@ class webScraper:
     def companyURL(self, baseURL):
         """
         FUNCTION: Scrapes Google for Company URLs
-        companyURL: an unfiltered list of company URLs; these may contain duplicate companies
+        companyURL: each scraped item in the Google search
+        companyURLs: an unfiltered list of company URLs; these may contain duplicate companies
         baseURL: the base URL of the ATS that will be scraped
         unfilteredURL: URL of company sites that will be scraped in Google
         r: filtered company URL; is of format <baseURL>/company
+        filteredURLs: a list of filtered URLs
         Writes the resulting scraped websites onto a different file, of format baseURL.txt
         """
         i = 1
-        companyURL = []
+        companyURLs = []
+        filteredURLs = []
         with open(baseURL + ".txt", "a+") as companies:
-            for unfilteredURL in search('site:' + atsURL, stop = 25000):
-                print(unfilteredURL)
+            for companyURL in search('site:' + atsURL, stop = 25000):
+                print(companyURL)
                 i+=1
                 if i % 10 == 0:
                     time.sleep(60)
-                companyURL.append(unfilteredURL)
-            for url in companyURL:
+                companyURLs.append(companyURL)
+            for url in companyURLs:
                 r = re.search(r"https:\/\/jobs\.lever\.co\/[^/]+\/?$", url)
                 if r is not None:
                     print("FILTERED! " + r.group(0))
-                    companies.write(r.group(0)+"\n")
-
+                    if r is not in filteredURLs:
+                        filteredURLs.append(r.group(0))
+                        companies.write(r.group(0)+"\n")
 for atsURL in atsURLs:
     webScraper().companyURL(atsURL)
