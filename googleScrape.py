6d085e72e4d0bf66b935f798e2f8eaea9a2eeb3c
aiarabelo <49424364+aiarabelo@users.noreply.github.com>
2019-07-06 17:56:47 -0700

    Updated function description, and fixed scraper's faulty for loop

--- googleScrape.py
+++ googleScrape.py
@@ -1,34 +1,33 @@
 from googlesearch import search
 import time
+import re
 
 atsURLs = ['boards.greenhouse.io', 'jobs.lever.co']
 
 class webScraper:
     def companyURL(self, baseURL):
         """
-        FUNCTION: Fills out the application page
-        questionPair: a dictionary containing questions as they key, 
-                    and the corresponding WebElements as values
-        userData: a dictionary containing the questions as the key, 
-                and the answers as valuesH
-        Returns questionPair, a dictionary containing questions and its corresponding webelement
+        FUNCTION: Scrapes Google for Company URLs
+        companyURL: an unfiltered list of company URLs; these may contain duplicate companies
+        baseURL: the base URL of the ATS that will be scraped
+        unfilteredURL: URL of company sites that will be scraped in Google
+        r: filtered company URL; is of format <baseURL>/company
+        Writes the resulting scraped websites onto a different file, of format baseURL.txt
         """
         i = 1
         companyURL = []
         with open(baseURL + ".txt", "a+") as companies:
             for unfilteredURL in search('site:' + atsURL, stop = 25000):
-                #companies.write(url+"\n")
+                print(unfilteredURL)
                 i+=1
                 if i % 10 == 0:
                     time.sleep(60)
-                #for url in unfilteredURL:
+                companyURL.append(unfilteredURL)
+            for url in companyURL:
                 r = re.search(r"https:\/\/jobs\.lever\.co\/[^/]+\/?$", url)
                 if r is not None:
-                    print(url)
-                    companyURL.append(url)
-            
-
-                
+                    print("FILTERED! " + r.group(0))
+                    companies.write(r.group(0)+"\n")
 
 for atsURL in atsURLs:
     webScraper().companyURL(atsURL)
