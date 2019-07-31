d0b0a462c6e3e946f65f9e08e4b617ec91f7ce49
aiarabelo <49424364+aiarabelo@users.noreply.github.com>
2019-07-31 00:39:08 -0700

    Fixed problems in driver

--- driver.py
+++ driver.py
@@ -2,7 +2,7 @@ import os
 from googleScrape import WebScraper
 from agents import Agent, LeverAgent
 import time
-from models import Company
+from models import Company, CompanyJobs
 from createEngine import createSession
 import psycopg2
 import json
@@ -16,11 +16,22 @@ if __name__ == "__main__":
     # Lists details of the job posting: commitment, title, and applyUrl
     companyDetails = {}
     filteredCompanyDetails = []
-    
-    # Getting company names that use the ATS
-    for atsURL in atsURLs:
-        WebScraper().getCompanyInfo(atsURL)
         
+    """
+    FUNCTION: Creates "companies" and "job_listings" tables 
+    """
+    session = createSession()
+    try:
+        Company.__table__.create(engine)
+        CompanyJobs.__table__.create(engine)
+        session.commit()
+        # Getting company names that use the ATS
+        for atsURL in atsURLs:
+            WebScraper().getCompanyInfo(atsURL)
+    except: 
+        print("Tables already created")
+    session.close()
+    
     # Hardcoded: Getting company names off the ATS
     session = createSession()
     leverCompanyNames = Company.getCompanyNames(session, "jobs.lever.co")
@@ -34,6 +45,7 @@ if __name__ == "__main__":
 
     # TODO: Generalize this for all ATS
     session = createSession()
+    i=0
     for companyName in leverCompanyNames:
         print("Extracting job postings from " + companyName + "...")
         jobPostList = WebScraper().listJobPosts(companyName)  # This is for Lever only
@@ -43,8 +55,12 @@ if __name__ == "__main__":
         except:
             print("Error for extracting details from " + companyName)
             pass
+
         print(companyDetails[companyName])
         print("Extraction complete for " + companyName + "!")
+        i += 1
+        if i % 10 == 0:
+            break
     session.close()
     
     filteredCompanyDetails.extend(WebScraper().filterCompany(session, commitment, title))
@@ -60,25 +76,23 @@ if __name__ == "__main__":
         userData = json.loads(f.read())
 
     # Fill out the job applications' easy questions
-    # TODO: Clear textbox before sending keys
+    # TODO: Clear textbox before sending keys, items should correspond to database entries
     for item in filteredCompanyDetails:
         leverCrawler = LeverAgent(False, "./chromedriver.exe")
-        print("Applying to " + item[3] + "...")
+        print("Applying to " + item + "...")
         try:
-            leverCrawler.autoInputQuestion(
-                leverCrawler.getQuestionDict(item[2]), userData
-            )
+            leverCrawler.autoInputQuestion(leverCrawler.getQuestionDict(item), userData)
         except:
             print(
                 "Error in application for "
-                + item[3]
+                + session.query(CompanyJobs.company_name)[0]
                 + ": "
-                + item[1]
+                + session.query(CompanyJobs.title)[0]
                 + " ("
-                + item[0]
+                + session.query(CompanyJobs.commitment)[0]
                 + ")."
             )
             pass
         time.sleep(60)
-
     session.close()
+    
